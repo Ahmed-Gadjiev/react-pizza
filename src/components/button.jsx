ad28703867ed28b8ce97020dc535c095b6904567
Ahmad <akhmed.34@mail.ru>
2022-02-18 21:39:58 +0300

    commit-4

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import classNames from 'classnames';
 
 function Button(props) {
-    const { outline, children, onClick, className } = props;
+    const { outline, children, onClick, className, cart } = props;
 
     return (
         <button
             onClick={onClick}
             className={classNames('button', className, {
                 'button--outline': outline,
+                'button--cart': cart
             })}
         >
             {children}
